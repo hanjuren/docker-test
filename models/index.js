68b0744b8d3ae8c79d352666a577ebf2c1244aed
hanjuren <juren528@gmail.com>
2021-12-30 18:52:47 +0900

    create User Models

--- models/index.js
+++ models/index.js
@@ -2,7 +2,6 @@ const Sequelize = require('sequelize');
 
 const env = 'development';
 const config = require('../config/config.json')[env];
-console.log(config)
 const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
